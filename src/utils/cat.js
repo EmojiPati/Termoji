08a1692aad59c0abe0b4ef5c8cb288fa0a463f52
EmojiPati <49492351+EmojiPati@users.noreply.github.com>
2023-04-15 14:07:51 +0300

    More Cat.

--- src/utils/cat.js
+++ src/utils/cat.js
@@ -1,5 +1,5 @@
 export default async function getcat() {
-  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const res = await fetch("https://api.thecatapi.com/v1/images/search?mime_types=gif");
   const resp = await res.json();
   return resp[0].url;
 }
